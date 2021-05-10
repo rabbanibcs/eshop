98231eda4495a32f5ed08a6f5b5ec660b51992f7
rabbanibcs <rabbanibcs@gmail.com>
2021-05-11 02:53:13 +0600

    initial commit

--- eshop/urls.py
+++ eshop/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from shop.views import ProductView
+from shop.views import ProductsView,OrderView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/',ProductView.as_view(),name='shop'),
+    path('shop/',ProductsView.as_view(),name='shop'),
+    path('order/', OrderView.as_view(), name='order'),
 
 
 ]
