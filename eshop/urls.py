566a8fba08427d045baa60959c48850777de6f42
rabbanibcs <rabbanibcs@gmail.com>
2021-05-12 03:13:20 +0600

    final commit

--- eshop/urls.py
+++ eshop/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from shop.views import ProductsView,OrderView,CartView
+from shop.views import ProductsView,OrderView,CartView,order_pdf_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/',ProductsView.as_view(),name='shop'),
     path('order/', OrderView.as_view(),),
+    path('order/download/',order_pdf_view, ),
     path('cart/', CartView.as_view(), name='cart'),
 
 
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,14 @@
 from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.template.loader import get_template
 from django.views import View
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
+from django.conf import settings
+import os
+
+
 from shop.models import Product, Category, Customer, Cart, Order
 
 
@@ -9,17 +16,17 @@ class ProductsView(View):
 
     def get(self, request):
         category = Category.objects.all()
+
+        # search functionality. by product name.
         if request.GET.get('query'):
-            query=request.GET.get('query')
+            query = request.GET.get('query')
             products = Product.objects.filter(name__startswith=query)
             if products:
                 pass
             else:
                 messages.warning(request, 'No product found.')
-
         else:
             products = Product.objects.all()
-
         context = {'products': products, 'category': category}
         return render(request, 'shop/shop.html', context)
 
@@ -31,7 +38,6 @@ class ProductsView(View):
             products = Product.objects.filter(category_id=category_id)
             # product_id = Product.objects.filter(category_id=category_id)
             context = {'products': products}
-            print('category_id', category_id)
             return render(request, 'shop/shop.html', context)
 
         # search for products the given category
@@ -40,39 +46,32 @@ class ProductsView(View):
             print('product_id', product_id)
             product = Product.objects.filter(id=product_id)
             stock = Product.objects.get(pk=product_id).stock
-            print(stock)
             context = {'stock': stock, 'products': product, 'id': product_id}
             return render(request, 'shop/shop.html', context)
 
-        # input quantity from user
+        # take input from user and add to cart
         if request.POST.get('quantity'):
             product_id = request.POST.get('id')
             quantity = request.POST.get('quantity')
             stock = Product.objects.get(pk=product_id).stock
 
-            print('quantity', quantity)
-            print('product_id', product_id)
-
             if int(quantity) > stock:
                 messages.error(request, 'Sorry, Quantity is more than Stock')
             else:
                 self.add_to_cart(request, product_id, quantity)
-
             category = Category.objects.all()
             products = Product.objects.all()
             context = {'category': category, 'products': products}
 
             return render(request, 'shop/shop.html', context)
 
+        # add to cart from product view
         if request.POST.get('product'):
             product_id = request.POST.get('product')
-
-            print(product_id)
             self.add_to_cart(request, product_id)
-
             return redirect('shop')
 
-
+    # add to cart functionality
     @staticmethod
     def add_to_cart(request, product_id, quantity='1'):
         pk = product_id
@@ -80,24 +79,20 @@ class ProductsView(View):
         cart = request.session.get('cart')
         if cart:
             quantity = cart.get(pk)
-            # print(quantity)
             if quantity:
                 cart[pk] = (q + quantity)
             else:
                 cart[pk] = q
         else:
             cart = {pk: q}
-        print(cart)
         request.session['cart'] = cart
         messages.success(request, 'Added to the Cart. ')
 
 
 class CartView(View):
-
+    # view of the cart
     def get(self, request):
-
         cart = request.session.get('cart')
-        print(cart)
         products = {}
         if cart:
             for product_id, quantity in cart.items():
@@ -107,15 +102,14 @@ class CartView(View):
         context = {'products': products, }
         return render(request, 'shop/cart.html', context)
 
+    # delete a product from cart
     def post(self, request):
         if request.POST.get('product'):
             pro_id = request.POST.get('product')
             del request.session['cart'][pro_id]
         request.session.save()
         cart = request.session.get('cart')
-        print(cart)
         products = {}
-
         for product_id, quantity in cart.items():
             product = Product.objects.get(pk=product_id)
             products[product] = quantity
@@ -130,7 +124,6 @@ class OrderView(View):
             name = request.POST.get('name')
             phone = request.POST.get('phone')
             email = request.POST.get('email')
-
             customer = Customer(name=name, phone=phone, email=email)
             customer.save()
             qrcode_path = customer.qrcode_create()
@@ -150,10 +143,55 @@ class OrderView(View):
         return render(request, 'shop/order.html', context)
 
     def get(self, request):
-
-        order = Order.objects.get(pk=20)
+        order = Order.objects.get(pk=24)
         cart = order.cart_set.all()
-        print(cart)
         context = {'cart': cart, 'order': order}
         return render(request, 'shop/order.html', context)
 
+
+def order_pdf_view(request):
+    order = Order.objects.get(pk=24)
+    cart = order.cart_set.all()
+    context = {'cart': cart, 'order': order}
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    template = get_template('shop/order.html')
+    html = template.render(context)
+
+    pisa_status = pisa.CreatePDF(
+        html, dest=response, link_callback=link_callback)
+    if pisa_status.err:
+        return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+
+def link_callback(uri, rel):
+    """
+    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+    resources
+    """
+    result = finders.find(uri)
+    if result:
+        if not isinstance(result, (list, tuple)):
+            result = [result]
+        result = list(os.path.realpath(path) for path in result)
+        path = result[0]
+    else:
+        sUrl = settings.STATIC_URL  # Typically /static/
+        sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+        mUrl = settings.MEDIA_URL  # Typically /media/
+        mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+
+        if uri.startswith(mUrl):
+            path = os.path.join(mRoot, uri.replace(mUrl, ""))
+        elif uri.startswith(sUrl):
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+        else:
+            return uri
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+        raise Exception(
+            'media URI must start with %s or %s' % (sUrl, mUrl)
+        )
+    return path
